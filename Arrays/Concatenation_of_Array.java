7f07d545aa82aa3a17ead20c8605ba377e4612a1
Paridhi Tayal <paridhitayal6@gmail.com>
2026-03-17 09:54:17 +0530

    Leetcode 1929. Concatenation of Array

---
+++ Arrays/Concatenation_of_Array.java
@@ -0,0 +1,13 @@
+class Solution
+{
+    public int[] getConcatenation(int[] nums) 
+    {
+        int[] a= new int[nums.length*2];
+        for(int i=0;i<nums.length;i++)
+        {
+            a[i]=nums[i];
+            a[i+nums.length]=nums[i];
+        }
+        return a;
+    }
+}
