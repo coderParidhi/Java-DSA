496a289be2fc3f498a5ea3da09aa4e4cf1f76e5d
Paridhi Tayal <paridhitayal6@gmail.com>
2026-03-21 19:10:10 +0530

    LeetCode 136: Single Number

---
+++ Hashing/HashSet/single_number.java
@@ -0,0 +1,15 @@
+class Solution
+{
+    public int singleNumber(int[] nums) 
+    {
+        HashSet<Integer>s=new HashSet<>();
+        for (int i=0;i<nums.length;i++)
+        {
+            if(s.contains(nums[i]))
+            s.remove(nums[i]);
+            else
+            s.add(nums[i]);
+        }   
+        return s.iterator().next(); 
+    }
+}
