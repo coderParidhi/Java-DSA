d5b43d1909f359ddee1e60303af6889ad30ffb0a
Paridhi Tayal <paridhitayal6@gmail.com>
2026-03-21 20:08:54 +0530

    LeetCode 202: Happy Number

---
+++ Arrays/Happy_Number.java
@@ -0,0 +1,21 @@
+class Solution 
+{
+    public boolean isHappy(int n) 
+    {
+       while(n>9)
+       {
+        int r=0;
+        while(n>0)
+        {
+            r=r+((n%10)*(n%10));
+            n=n/10;
+        }
+        n=r;
+       }  
+       if(n==1 || n==7)
+       return true;
+       else
+       return false;
+    }
+    
+}
