c276c9052fc4ca5cff08101e8c9f9b39b2cd803b
Paridhi Tayal <paridhitayal6@gmail.com>
2026-03-31 09:37:28 +0530

    Leetcode 169. Majority Element

--- Arrays/Majority_element.java
+++ Arrays/Majority_element.java
@@ -1,1 +1,29 @@
-class Solution {    public int majorityElement(int[] nums)     {        int candidate = 0;        int count = 0;        for (int num : nums)         {            if (count == 0)             {                candidate = num;            }            if (num == candidate)             {                count++;            } else             {                count--;            }        }        return candidate;    }}
+import java.util.*;
+class Solution 
+{
+    public int majorityElement(int[] nums) 
+    {
+       HashMap<Integer,Integer> map=new HashMap<>();
+       for(int n:nums)
+       {
+        map.put(n,map.getOrDefault(n,0)+1);
+        if(map.get(n)>nums.length/2)
+        return n;
+       }
+       return -1;
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        Solution obj=new Solution();
+        System.out.println("Enter the size of array");
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        System.out.println("Enter the elements of array");
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("The majority element is: "+ obj.majorityElement(arr));
+    }
+}
